6a601e75e6f08ef6f249b54bcddadc6981ebf997
Maria E <40209515+MariaERR@users.noreply.github.com>
2023-07-18 18:06:43 +0100

    Update index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,11 +1,1 @@
-const fs = require('fs');
 
-const folderList = './src/Features/tas-apis';
-
-const folders = fs.readdirSync(folderList, { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
-
-console.log('Folders:', folders.join(', '));
-
-process.stdout.write(JSON.stringify(folders));
