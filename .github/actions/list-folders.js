dc9a42404c9703ff1ef04c7e7d0719e86d06601f
Maria E <40209515+MariaERR@users.noreply.github.com>
2023-07-18 17:51:39 +0100

    Update and rename list-folders.yaml to list-folders.js

---
+++ .github/actions/list-folders.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const folderList = './src/Features/tas-apis';
+
+const folders = fs.readdirSync(folderList, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+console.log(JSON.stringify(folders));
