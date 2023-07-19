1b9b42b2c5efdd5fbad59e3c0e07e40fc92e1800
Maria E <40209515+MariaERR@users.noreply.github.com>
2023-07-19 14:20:28 +0100

    Update index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,1 +1,7 @@
 
+const core = require('@actions/core');
+
+const folderNames = core.getInput('folder-names');
+const folders = folderNames.split(',');
+
+core.setOutput('selected_folders', JSON.stringify(folders));
