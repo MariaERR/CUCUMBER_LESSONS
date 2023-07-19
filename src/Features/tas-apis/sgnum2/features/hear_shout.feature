a0d6f9566c69bbf722a2dbd653dcea9bbde15f37
MariaERR <errfig.m@gmail.com>
2023-07-19 13:41:56 +0100

    update code

--- src/Features/tas-apis/sgnum2/features/hear_shout.feature
+++ src/Features/tas-apis/sgnum2/features/hear_shout.feature
@@ -1,5 +1,5 @@
-Feature: Hear shout
-	Scenario: Listener in within range
-		Given Lucy is located 15 metres from Sean
-		When Sean shout "free bagels at Sean's"
-		Then Lucy hears Sean's message
+Feature: Hear shout1
+	Scenario: Listener1 in within range
+		Given Lucy1 is located 15 metres from Sean
+		When Sean1 shout "free bagels at Sean's"
+		Then Lucy1 hears Sean's message
--- src/Features/tas-apis/sgnum2/features/step_definitions/step.js
+++ src/Features/tas-apis/sgnum2/features/step_definitions/step.js
@@ -3,7 +3,7 @@ const Person= require ('../../../shouty.js')
 const {Given, When, Then} = require ('@cucumber/cucumber')
 const {assertThat, is}=require ('hamjest')
 
-  Given('Lucy is located {int} metres from Sean', function (distance) {
+  Given('Lucy1 is located {int} metres from Sean', function (distance) {
   this.lucy=new Person
 	this.sean=new Person
 	this.lucy.moveTo(distance)
@@ -11,11 +11,11 @@ const {assertThat, is}=require ('hamjest')
 	});
 
 
-  When('Sean shout {string}', function (message) {
+  When('Sean1 shout {string}', function (message) {
 this.sean.shout(message)  
 this.message=message
 });
 
-  Then('Lucy hears Sean\'s message', function () {
+  Then('Lucy1 hears Sean\'s message', function () {
 assertThat(this.lucy.messaheHeard(), is([this.message]))
  });
