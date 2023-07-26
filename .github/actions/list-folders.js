44ab17e42f34971cf10dfab14b456fb1bf556e9b
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 11:00:26 +0100

    Update list-folders.js

--- .github/actions/list-folders.js
+++ .github/actions/list-folders.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
-const folderList = './src/Features/tas-apis';
+const folderList = './CUCUMBER_LESSONS/src/Features/tas-apis';
 
 const folders = fs.readdirSync(folderList, { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory())
   .map((dirent) => dirent.name);
 
-console.log('Folders:', folders.join(', '));
-
-process.stdout.write(JSON.stringify(folders));
+console.log(JSON.stringify(folders));
