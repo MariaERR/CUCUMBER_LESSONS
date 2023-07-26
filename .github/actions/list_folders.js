6a8c3f629d8fcbd53fc313f53c4a3652bde31096
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 17:16:47 +0100

    Update list_folders.js

--- .github/actions/list_folders.js
+++ .github/actions/list_folders.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
-const path = require('path');
 
-const folderList = './CUCUMBER_LESSONS/src/Features/tas-apis';
+try {
+  // Read the JSON file
+  const data = fs.readFileSync('.github/actions/folders_config.json', 'utf8');
+  const config = JSON.parse(data);
 
-const folders = fs.readdirSync(folderList, { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => path.join(folderList, dirent.name));
+  // Access the folders array
+  const folders = config.folders;
 
-console.log(JSON.stringify(folders));
+  // Process the folders array or extract other data as needed
+  // For this example, we'll just print the selected folders
+  const selectedFolders = folders.filter((folder) => folder.selected).map((folder) => folder.name);
+  console.log(selectedFolders.join(','));
+
+} catch (err) {
+  console.error('Error reading or parsing the JSON file:', err);
+  process.exit(1);
+}
