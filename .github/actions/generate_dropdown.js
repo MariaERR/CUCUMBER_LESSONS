2ad084d0400bca4a238e28860d21305df0cec7db
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 11:57:19 +0100

    Update generate_dropdown.js

--- .github/actions/generate_dropdown.js
+++ .github/actions/generate_dropdown.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const folderListPath = path.join(__dirname, '..', '..', '..', 'folders_list.json');
+const folderListPath = path.join(process.env.GITHUB_WORKSPACE, 'folders_list.json');
 const folders = require(folderListPath).folders;
 
 const options = folders.map((folder) => ({ "value": folder, "label": folder }));
