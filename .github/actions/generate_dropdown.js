b4cb99538411e8e229a53565bd5124be86a280df
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 11:47:15 +0100

    Update generate_dropdown.js

--- .github/actions/generate_dropdown.js
+++ .github/actions/generate_dropdown.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const folderList = './folders_list.json';
 const folders = require(folderList).folders;
 
-const options = folders.map((folder) => ({ label: folder, value: folder }));
+const options = folders.map((folder) => ({ "value": folder, "label": folder }));
 
 console.log(JSON.stringify(options));
