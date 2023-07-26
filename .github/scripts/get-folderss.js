3296de4cfa30a6facf606ca300f4c11c6d9d4547
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 10:16:26 +0100

    Update and rename checkbox-folders.js to get-folderss.js

---
+++ .github/scripts/get-folderss.js
@@ -0,0 +1,8 @@
+// get_folders.js
+async function getFolders() {
+  const response = await fetch('https://api.github.com/repos/MariaERR/CUCUMBER_LESSONS/contents/src/Features/tas-apis');
+  const data = await response.json();
+  
+  const folders = data.filter(item => item.type === 'dir').map(item => item.name);
+  return folders;
+}
