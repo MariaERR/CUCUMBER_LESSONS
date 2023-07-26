fe6e1b522a17d8a539d448dfad821b903211b0d8
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 10:28:40 +0100

    Update get_folders.js

--- .github/scripts/get_folders.js
+++ .github/scripts/get_folders.js
@@ -1,8 +1,10 @@
-// get_folders.js
+// scripts/get_folders.js
 async function getFolders() {
   const response = await fetch('https://api.github.com/repos/MariaERR/CUCUMBER_LESSONS/contents/src/Features/tas-apis');
   const data = await response.json();
   
   const folders = data.filter(item => item.type === 'dir').map(item => item.name);
-  return folders;
+  return folders.join('\n');
 }
+
+console.log(await getFolders());
