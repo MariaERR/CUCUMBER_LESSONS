57af43362d0611c4a290644cacdd3a52157680b7
MariaE <40209515+MariaERR@users.noreply.github.com>
2023-07-26 11:54:52 +0100

    Update generate_dropdown.js

--- .github/actions/generate_dropdown.js
+++ .github/actions/generate_dropdown.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const folderList = path.join(__dirname, '..', '..', 'folders_list.json');
-const folders = require(folderList).folders;
+const folderListPath = path.join(__dirname, '..', '..', '..', 'folders_list.json');
+const folders = require(folderListPath).folders;
 
 const options = folders.map((folder) => ({ "value": folder, "label": folder }));
 
 fs.writeFileSync('./options.json', JSON.stringify(options));
-
